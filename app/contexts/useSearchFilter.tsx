523848d3f8792789be62fa16096683a86798bbf6
Maik Emanoel <maikemanoel11@gmail.com>
2024-04-09 10:07:33 -0300

    feat: handle with search filter

---
+++ app/contexts/useSearchFilter.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ReactNode, createContext, useContext, useState } from "react";
+import useDebounceValue from "../hooks/useDebounceValue";
+
+interface SearchFilterContextSchema {
+  debouncedSearch: string;
+  handleSearchFilter: (value: string) => void;
+}
+
+export const SearchFilterContext = createContext<
+  SearchFilterContextSchema | undefined
+>(undefined);
+
+export function SearchFilterProvider({ children }: { children: ReactNode }) {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounceValue(search, 250)
+
+  function handleSearchFilter(value: string) {
+    setSearch(value);
+  }
+
+  return (
+    <SearchFilterContext.Provider value={{ debouncedSearch, handleSearchFilter }}>
+      {children}
+    </SearchFilterContext.Provider>
+  );
+}
+
+export function useSearchFilter() {
+  const context = useContext(SearchFilterContext);
+
+  if (context === undefined) {
+    throw new Error("UseSearchFilter must be inside of a SearchFilterProvider");
+  }
+
+  return context;
+}
