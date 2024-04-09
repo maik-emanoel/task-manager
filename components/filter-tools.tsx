66abed01f065edc9036e77f32ee80456f103aafc
Maik Emanoel <maikemanoel11@gmail.com>
2024-04-09 10:05:17 -0300

    feat: catch filter-input value

--- components/filter-tools.tsx
+++ components/filter-tools.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { Input } from "./ui/input";
-import  StatusCombobox  from "./status-combobox";
+import StatusCombobox from "./status-combobox";
 import PriorityCombobox from "./priority-combobox";
+import { useSearchFilter } from "@/app/contexts/useSearchFilter";
 
 export default function FilterTools() {
+  const { handleSearchFilter } = useSearchFilter();
+
   return (
     <div className="flex gap-2">
-      <Input placeholder="Filter tasks..." name="filter-task-input" />
+      <Input
+        placeholder="Filter tasks..."
+        name="filter-input"
+        onChange={(e) => handleSearchFilter(e.target.value)}
+      />
       <StatusCombobox />
       <PriorityCombobox />
     </div>
