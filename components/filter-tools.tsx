bb5eb84dad71c8e7eb9d1e5ef69b62f993695a58
Maik Emanoel <maikemanoel11@gmail.com>
2024-04-29 13:44:54 -0300

    style: add magnifyingGlass icon

--- components/filter-tools.tsx
+++ components/filter-tools.tsx
@@ -4,17 +4,23 @@ import { Input } from "./ui/input";
 import StatusCombobox from "./status-combobox";
 import PriorityCombobox from "./priority-combobox";
 import { useSearchFilter } from "@/app/contexts/useSearchFilter";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
 export default function FilterTools() {
   const { handleSearchFilter } = useSearchFilter();
 
   return (
     <div className="flex gap-2">
-      <Input
-        placeholder="Filter tasks..."
-        name="filter-input"
-        onChange={(e) => handleSearchFilter(e.target.value)}
-      />
+      <div className="relative">
+        <Input
+          placeholder="Filter tasks..."
+          name="filter-input"
+          onChange={(e) => handleSearchFilter(e.target.value)}
+          className="pl-10 peer"
+        />
+        <MagnifyingGlass size={16} className="absolute top-1/2 -translate-y-1/2 left-3 text-muted-foreground peer-focus:text-primary" />
+      </div>
+
       <StatusCombobox />
       <PriorityCombobox />
     </div>
